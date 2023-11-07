31604dc1bd49a7c390eb474d4edb21c0661565fc
sam <slcaroquiroga@gmail.com>
2023-11-06 22:21:56 -0500

    validaciones form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,6 @@
-/* eslint-disable no-sequences */
+/* eslint-disable no-restricted-globals */
 import { useForm } from 'react-hook-form'
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from 'emailjs-com';
 import { Header } from "./Header";
 // import AnimationLogo from './AnimationLogo'
@@ -10,11 +10,13 @@ import github from '../assets/img/github.png'
 
 export const ContactForm = () => {
   const refForm = useRef()
-  const { register, formState: { errors }, handleSubmit } = useForm()
+  const { register, formState: { errors }, handleSubmit, reset } = useForm()
+  const [sendConfirmation, setSendConfirmation] = useState(false)
 
-   const onSubmit = (data) => {
-   console.log(data)
-  // }
+  const onSubmit = () => {
+    //  const onSubmit = (data) => {
+    // console.log(data)
+
 
     // const serviceId = 'service_mwbvb1b'
     // const templateId = 'template_mu21r7h'
@@ -23,6 +25,8 @@ export const ContactForm = () => {
     emailjs.sendForm('service_mwbvb1b', 'template_mu21r7h', refForm.current, '1za7EVodQy9VBMn1-')
       .then((result) => {
         console.log(result.text);
+        reset()
+        setSendConfirmation(true)
       }, (error) => {
         console.log(error.text);
       });
@@ -36,25 +40,25 @@ export const ContactForm = () => {
 
         {/* <AnimationLogo /> */}
 
-        <form ref={refForm} onSubmit={handleSubmit(onSubmit)} className='bg-white h-[400px] sm:h-[300px] w-[210px] p-4 ml-[100px] text-Roboto shadow-md rounded-sm sm:w-[500px] sm:h-[700px] sm:p-12 sm:ml-[600px] sm:mt-[-250px]'>
+        <form ref={refForm} onSubmit={handleSubmit(onSubmit)} className='bg-white h-[400px] sm:h-[300px] w-[210px] p-4 ml-[100px] text-Roboto shadow-md rounded-sm sm:w-[500px] sm:h-[700px] sm:pt-20 sm:ml-[600px] sm:mt-[-200px]'>
           <div className='flex flex-col shadow-md mb-10 text-[12px] sm:mb-12'>
-          <input name='username' id='username' type='text' className='sm:h-12 sm:text-[16px]'  {...register('name', {
-  required: true,
-  maxLength: 10,
-})}
+            <input name='username' id='username' type='text' className='sm:h-12 sm:text-[16px]' {...register('name', {
+              required: true,
+              maxLength: 10,
+            })}
 
               placeholder='NAME'
               style={{
                 color: '#B0B0B0',
                 fontSize: '0.8rem',
                 margin: '4px',
-              }}
-            />
+              }} />
             {errors.name?.type === 'required' && <p className='text-black'>Name is required</p>}
             {errors.name?.type === 'maxLength' && <p className='text-black'>Name must be less than 10 characters</p>}
           </div>
           <div className='flex flex-col shadow-md mb-10 text-[12px] sm:mb-12 '>
             <input name="useremail" id='useremail' className='sm:h-12 sm:text-[16px]' type='text' {...register('useremail', {
+              required: true,
               pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i // nombre@dominio.com
             })}
               placeholder='EMAIL'
@@ -62,9 +66,8 @@ export const ContactForm = () => {
                 color: '#B0B0B0',
                 fontSize: '0.8rem',
                 margin: '4px',
-              }}
-            />
-            {errors.email?.type === 'pattern' && <p>Please enter a valid Email</p>}
+              }} />
+            {errors.useremail?.type === 'pattern' && <p>Please enter a valid Email</p>}
           </div>
           <div className='flex flex-col shadow-md mb-10 text-[12px] sm:mb-12'>
             <input name="message" className='sm:h-12 sm:text-[16px]' type='text' {...register('message', {
@@ -75,11 +78,12 @@ export const ContactForm = () => {
                 color: '#B0B0B0',
                 fontSize: '0.8rem',
                 margin: '4px',
-              }}
-            />
+              }} />
             {errors.message?.type === 'required' && <p className='text-black'>Enter a message</p>}
           </div>
-          <input className='text-[12px] bg-zinc-300 w-[180px] h-6 rounded-sm sm:mb-12 sm:h-12 sm:w-[400px] hover:text-white hover:shadow-md cursor-pointer' type='submit' value='SEND' />
+
+          <input name='send' className='text-[12px] bg-zinc-300 w-[180px] h-6 rounded-sm sm:mb-12 sm:h-12 sm:w-[400px] hover:text-white hover:shadow-md cursor-pointer' type='submit' value='SEND' />
+          {sendConfirmation && <p className='text-[12px]'>Thank You {name}!</p>}
           <p className='mt-12 text-[10px] text-center sm:text-[14px]'>
             You can also write to my email
             <a
@@ -100,8 +104,7 @@ export const ContactForm = () => {
             </a>
           </div>
         </form>
-      </article>
-    </>
+      </article></>
   )
 }
 
