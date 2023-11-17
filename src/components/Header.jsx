f9dc85ec81c9c2345b6722ccdb11c8af9f90b242
sam <slcaroquiroga@gmail.com>
2023-11-17 01:19:13 -0500

    spanish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,19 +14,19 @@ export const Header = () => {
 
 
   return (
-    <header id='Landing-header' className='Header flex justify-between items-center'>
-       <img className='Header-logo opacity-0 mt-1 ml-[50px] w-[50px] h-[50px] sm:ml-14' src={planta} alt="Logo SC" />
+    <header id='Landing-header' className='Header flex justify-between items-center sm:mr-[-30px]'>
+       <img className='Header-logo opacity-0 mt-1 ml-[30px] w-[50px] h-[50px]' src={planta} alt="Logo SC" />
 
       <AnimmetionLogo />
 
-      <div className='flex flex-col items-center mr-20 mt-[1px] sm:ml-20 sm:flex-row'>
+      <div className='flex flex-col items-center mr-20 mt-[1px] sm:flex-row'>
         <button className='Header-button sm:hidden' onClick={toggleMenu}>
 
           <img className={`Header-menu w-10 h-10 cursor-pointer ${menu ? 'opacity-1' : ''}`} src={burgerMenu} alt='Menu Hamburguesa' />
         </button>
 
         <nav className={`Header-nav sm:mt-[-100px] sm:flex sm:bg-withe sm:text-black ${menu ? 'isActive w-100  bg-white text-GrayishBlue p-10' : ''}`}>
-          <ul className=' Header-ul flex flex-col text-[16px] sm:w-[400px] sm:text-[18px] sm:place-content-around sm:items-center sm:flex-row sm:space-x-4 sm:ml-50'>
+          <ul className=' Header-ul flex flex-col text-[14px] sm:w-[500px] sm:text-[18px] sm:place-content-around sm:items-center sm:flex-row sm:space-x-4 sm:ml-50'>
             <Link to='/' className='hover:text-zinc-300' >
               INICIO
             </Link>
